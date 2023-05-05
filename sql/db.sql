48d4abb673407a79a5cb2efa425f2c706c4f5207
Cesar Osimani <cosimani@ubp.edu.ar>
2023-05-05 15:41:42 -0300

    .

--- sql/db.sql
+++ sql/db.sql
@@ -12,4 +12,4 @@ CREATE TABLE `osi_usuarios` (
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 
 INSERT INTO `osi_usuarios` ( `usuario`, `clave`, `nombre`, `apellido`, `mail` ) VALUES
-( 'admin', '1234', 'César', 'Osimani', 'cesarosimani@gmail.com' );
+( 'cosimani', '1234', 'César', 'Osimani', 'cesarosimani@gmail.com' );
