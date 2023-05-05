c2d018df30e15f346bd48cccf2120a558dba0305
Cesar Osimani <cosimani@ubp.edu.ar>
2023-05-05 15:08:22 -0300

    .

---
+++ sql/db.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS `osi_usuarios`;
+
+CREATE TABLE `osi_usuarios` (
+  `usuario` varchar(50) NOT NULL,
+  `clave` varchar(50) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellido` varchar(50) DEFAULT NULL,
+  `mail` varchar(100) DEFAULT NULL,
+
+  PRIMARY KEY ( `usuario` ) USING BTREE
+
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+INSERT INTO `osi_usuarios` ( `usuario`, `clave`, `nombre`, `apellido`, `mail` ) VALUES
+( 'admin', '1234', 'César', 'Osimani', 'cesarosimani@gmail.com' );
